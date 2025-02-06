bbb0338261f1673c69a467f8336e9c6015413083
Matthew Nakamura <matthew.t.nakamura@gmail.com>
2025-02-05 20:25:27 -1000

    removed 3.9 support

--- noxfile.py
+++ noxfile.py
@@ -8,7 +8,7 @@ from nox.sessions import Session
 session = nox.session
 
 package = "AM_Creep_Analysis"
-python_versions = ["3.12", "3.11", "3.10", "3.9"]
+python_versions = ["3.12", "3.11", "3.10"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = ("docs-build", "docs", "mypy")
 
