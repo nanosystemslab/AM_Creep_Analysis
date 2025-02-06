259554da3de4840cdd94c8c26e658144c3f4fef9
Matthew Nakamura <matthew.t.nakamura@gmail.com>
2025-02-05 20:40:46 -1000

    updated nox

--- noxfile.py
+++ noxfile.py
@@ -26,7 +26,7 @@ def docs_build(session: Session) -> None:
         args.insert(0, "--color")
 
     session.install(".")
-    session.install("sphinx", "sphinx-argparse", "furo", "myst-parser")
+    session.install("sphinx", "sphinx-argparse", "furo", "myst-parser", "sphinx-click")
 
     build_dir = Path("docs", "_build")
     if build_dir.exists():
